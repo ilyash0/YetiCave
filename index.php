f4ddafc1debc80c49e34646d11604b47c4e7a8f9
Ilya Shmyryov <119329448+ilyash0@users.noreply.github.com>
2025-09-15 20:56:45 +0500

    Revert "return config data"
    This reverts commit f27539bac867d708b1881751908b52b7412fa71d.

--- index.php
+++ index.php
@@ -1,20 +1,19 @@
 <?php
 require_once("helpers.php");
 require_once("functions.php");
-$data = require_once("data.php");
+require_once("data.php");
 
-$page_content = include_template("main.php", [
-    "categories" => $data["categories"],
-    "products" => $data["products"]
-]);
+$page_content = include_template("main.php", ["categories" => $categories, 'products' => $products]);
 
-$layout_content = include_template("layout.php", [
-    "content" => $page_content,
-    "title" => "YetiCave",
-    "categories" => $data["categories"],
-    "user_name" => $data["user_name"],
-    "is_auth" => $data["is_auth"]
-]);
+$layout_content = include_template("layout.php",
+    [
+        "content" => $page_content,
+        "title" => "Заголовок",
+        "categories" => $categories,
+        "user_name" => $user_name,
+        "is_auth" => $is_auth
+    ]
+);
 
 print($layout_content);
 
